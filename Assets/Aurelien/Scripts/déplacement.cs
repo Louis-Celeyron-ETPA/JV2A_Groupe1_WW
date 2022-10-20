b2d8131b69aa372387410da7209aa0a2e316d06f
Aurelien Bonbony <aurelien.bonbony@gmail.com>
2022-10-20 10:11:23 +0200

    Test 2 Aurelien

--- Assets/Aurelien/Scripts/déplacement.cs
+++ Assets/Aurelien/Scripts/déplacement.cs
@@ -2,42 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class déplacement : MonoBehaviour
-{
-    public KeyCode myKey;
-    public Transform thierry;
-    public Transform Mélanie;
-    public float speedMax = 1000f;
-    // Start is called before the first frame update
-    void Start()
+namespace Aurelien {
+    public class déplacement : MonoBehaviour
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.Z) == true)
+        public KeyCode myKey;
+        public Transform thierry;
+        public Transform Mélanie;
+        public float speedMax = 1000f;
+        // Start is called before the first frame update
+        void Start()
         {
-            thierry.position += thierry.forward * speedMax;
+
         }
 
-        if (Input.GetKey(KeyCode.Q) == true)
+        // Update is called once per frame
+        void Update()
         {
-            thierry.position += thierry.right * -1 * speedMax;
+            if (Input.GetKey(KeyCode.Z) == true)
+            {
+                thierry.position += thierry.forward * speedMax;
+            }
+
+            if (Input.GetKey(KeyCode.Q) == true)
+            {
+                thierry.position += thierry.right * -1 * speedMax;
+            }
+
+            if (Input.GetKey(KeyCode.S) == true)
+            {
+                thierry.position += thierry.forward * -1 * speedMax;
+            }
+
+            if (Input.GetKey(KeyCode.D) == true)
+            {
+                thierry.position += thierry.right * speedMax;
+            }
+
+
+
         }
-
-        if (Input.GetKey(KeyCode.S) == true)
-        {
-            thierry.position += thierry.forward * -1 * speedMax;
-        }
-
-        if (Input.GetKey(KeyCode.D) == true)
-        {
-            thierry.position += thierry.right * speedMax;
-        }
-
-
-
     }
 }
--- Assets/Aurelien/Scripts/rotation.cs
+++ Assets/Aurelien/Scripts/rotation.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class rotation : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+namespace Aurelien {
+    public class rotation : MonoBehaviour
     {
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.eulerAngles += transform.up * 0.8f;
+        // Update is called once per frame
+        void Update()
+        {
+            transform.eulerAngles += transform.up * 0.8f;
+        }
     }
 }
