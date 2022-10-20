43322e76446b4a66cf68edc8178a7f4dbf5bb25d
Lisa Garaud <lisa.kalithal@gmail.com>
2022-10-20 10:23:45 +0200

    Sauvegarde 1

---
+++ Assets/Lisa/Scripts/BunnyMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform move;
+
+    float speed = 0.1f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D) == true)
+        {
+            //move.position += move.right;
+            transform.localPosition += transform.right * speed;
+        }
+        if (Input.GetKey(KeyCode.Q) == true)
+        {
+            //move.position += move.forward;
+            transform.localPosition += transform.right * -speed;
+        }
+    }
+}
