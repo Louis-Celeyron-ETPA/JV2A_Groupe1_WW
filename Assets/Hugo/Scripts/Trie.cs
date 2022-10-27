ba68a5b18c73379515264d470b907a9989063117
RENNES\h.thomas <h.thomas@rns.ecolescreatives.com>
2022-10-27 11:25:26 +0200

    finit separer

--- Assets/Hugo/Scripts/Trie.cs
+++ Assets/Hugo/Scripts/Trie.cs
@@ -7,6 +7,9 @@ public class Trie : MonoBehaviour
     public KeyCode myKey;
     public Transform thierry;
     public string couleur;
+    public Selecteur score;
+    public bool verifCouleur=false;
+    //public Selecteur scoreBleu;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +19,7 @@ public class Trie : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Q) == true)
+        /*if (Input.GetKey(KeyCode.Q) == true)
         {
             thierry.position = new Vector3(28, 1, -1);
             VerifieCouleur();
@@ -27,15 +30,23 @@ public class Trie : MonoBehaviour
             thierry.position = new Vector3(-28, 1, -1);
             VerifieCouleur();
         }
+        */
 
-           
-    }
-
-    private void VerifieCouleur()
-    {
-        if (thierry.position.x >= -17 && couleur == "rouge")
+        if (verifCouleur == false)
         {
-            //compt = compt + 1;
+            if (thierry.position.x <= -17 && couleur == "rouge")
+            {
+                //compt = compt + 1;
+                score.scoreRouge = score.scoreRouge + 1;
+                verifCouleur = true;
+        }
+            if (thierry.position.x >= 20 && couleur == "bleu")
+            {
+                //compt = compt + 1;
+                score.scoreBleu = score.scoreBleu + 1;
+                verifCouleur = true;
+            }
         }
     }
+
 }
