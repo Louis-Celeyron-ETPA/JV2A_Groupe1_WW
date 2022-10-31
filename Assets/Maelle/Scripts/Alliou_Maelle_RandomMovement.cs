0fec373db23038da50adb4a8f76836e088dad5c0
RENNES\m.alliou <m.alliou@rns.ecolescreatives.com>
2022-10-31 16:48:09 +0100

    Sauvegarde_05_Alliou

--- Assets/Maelle/Scripts/Alliou_Maelle_RandomMovement.cs
+++ Assets/Maelle/Scripts/Alliou_Maelle_RandomMovement.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class Alliou_Maelle_RandomMovement : MonoBehaviour
 {
-    public float speed = 0.1f;
-    public Vector3 vel;
+    public float speed = 0.01f, rayon = 5;
+    public Vector3 target;
     // Start is called before the first frame update
     void Start()
     {
-
+        SetNewTarget();
     }
 
     // Update is called once per frame
     void Update()
     {
-        vel = Random.insideUnitSphere * speed;
-        vel.y = 0.1f;
-        transform.Translate(vel * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, target,speed);
+        if(Vector3.Distance(target, transform.position)<=0.5f)
+        {
+            SetNewTarget();
+        }
+    }
+
+    public void SetNewTarget()
+    {
+        var tempTarget = transform.position + Random.onUnitSphere * rayon;
+        target = new Vector3(tempTarget.x, transform.position.y, tempTarget.z);
+
     }
 }
