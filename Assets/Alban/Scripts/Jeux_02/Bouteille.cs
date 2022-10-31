109f418fba670cbed1b553348b4dfcf1e8133a2c
RENNES\a.motte <amotte@rns.ecolescreative.com>
2022-10-31 16:57:27 +0100

    Shooter

---
+++ Assets/Alban/Scripts/Jeux_02/Bouteille.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Alban
+{
+    public class Bouteille : MonoBehaviour
+    {
+
+        public Transform bottle;
+        public float bottleRemaining = 10;
+        public TextMesh bottles;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (bottleRemaining == 0)
+            {
+                Debug.LogError("Victoire !");
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            var bullet = FindObjectOfType<Tir>();
+            if (bullet != null)
+            {
+                Destroy(gameObject);
+                //bottleRemaining -= 1;
+            }
+        }
+    }
+}
