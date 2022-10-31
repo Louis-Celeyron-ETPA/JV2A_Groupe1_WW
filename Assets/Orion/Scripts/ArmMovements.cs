dec9b08b3a863f7c0366b7dc60c2f3c08a397bf4
RENNES\o.leclerc <o.leclerc@rns.ecolescreatives.com>
2022-10-31 16:57:14 +0100

    mouvement bras

--- Assets/Orion/Scripts/ArmMovements.cs
+++ Assets/Orion/Scripts/ArmMovements.cs
@@ -2,19 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ArmMovements : MonoBehaviour
-
+namespace Orion
 {
-    public Transform myTransform;
-    // Start is called before the first frame update
-    void Start()
-    {
-        myTransform.Rotate(new Vector3(0, 0, 90));
-    }
+    public class ArmMovements : MonoBehaviour
 
-    // Update is called once per frame
-    void Update()
     {
-        
+        public Transform myTransform;
+
+        public float rotateValue = 1f;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            myTransform.Rotate(new Vector3(0, 0, rotateValue));
+            if (myTransform.rotation.eulerAngles.z >= 90f)
+            {
+                rotateValue = rotateValue * -1f;
+            }
+
+        }
     }
 }
+
