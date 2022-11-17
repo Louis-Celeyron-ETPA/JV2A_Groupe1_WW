28c7b87cd5ae720018fef217f3bf9d85121240ce
RENNES\k.chasselin <kchasselin@gmail.com>
2022-11-17 11:30:48 +0100

    plume

--- Assets/Kylian/Scripts/plume.cs
+++ Assets/Kylian/Scripts/plume.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class plume : MonoBehaviour
 {
+    public Transform vole;
+    public KeyCode mykey;
+
+    public float speedMax = 0.01f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,16 @@ public class plume : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.D) == true)
+        {
+            transform.Translate(0.01f, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.Q) == true)
+        {
+            transform.Translate(-0.01f, 0, 0);
+        }
+
+        transform.Translate(0, 0, -0.3f * Time.deltaTime * 3);
     }
 }
