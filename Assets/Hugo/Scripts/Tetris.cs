7f4f8298bc9977f9839d9e404bad8e5dd0d550e9
RENNES\h.thomas <h.thomas@rns.ecolescreatives.com>
2022-11-17 11:28:17 +0100

    developpement 3ieme mini-jeux

--- Assets/Hugo/Scripts/Tetris.cs
+++ Assets/Hugo/Scripts/Tetris.cs
@@ -5,59 +5,67 @@ using UnityEngine;
 public class Tetris : MonoBehaviour
 {
     public KeyCode myKey;
-    public Transform thierry;
+    public Transform tetrominoSpawn;
     public float speedMax = 0.3f;
     public float acceleration = 0.1f;
     public float tempSpeedX = 0;
     public float rotationSpeed = 1;
     public bool canMove = false;
 
-    public List<GameObject> cubeSelect = new List<GameObject>();
-    int i;
+    public List<Tetromino> cubeSelectPrefab;
+    public Tetromino currentTetromino;
     public bool victoire = false;
+
+    private int index;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        GenerateTetromino();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (i < cubeSelect.Count)
+        if (Input.GetKey(KeyCode.D) == true)
         {
-            if (Input.GetKey(KeyCode.D) == true)
-            {
-                tempSpeedX += acceleration;
-                tempSpeedX = Mathf.Clamp(tempSpeedX, 0, speedMax);
-                cubeSelect[i].transform.position += Vector3.right * tempSpeedX;
-            }
-
-            if (Input.GetKey(KeyCode.Q) == true)
-            {
-                tempSpeedX += acceleration;
-                tempSpeedX = Mathf.Clamp(tempSpeedX, speedMax, 0);
-                cubeSelect[i].transform.position += Vector3.left * tempSpeedX;
-            }
-
-            if (cubeSelect[i].transform.position.y < -4)
-            {
-                i++;
-                actionSuivante();
-            }
+            tempSpeedX += acceleration;
+            tempSpeedX = Mathf.Clamp(tempSpeedX, 0, speedMax);
+            currentTetromino.transform.position += Vector3.right * tempSpeedX;
         }
+
+        if (Input.GetKey(KeyCode.Q) == true)
+        {
+            tempSpeedX += acceleration;
+            tempSpeedX = Mathf.Clamp(tempSpeedX, speedMax, 0);
+            currentTetromino.transform.position += Vector3.left * tempSpeedX;
+        }
+        
     }
 
 
-    private void actionSuivante()
+    private void GenerateTetromino()
     {
-        if (i < cubeSelect.Count)
+        if(index<10)
         {
-            cubeSelect[i].transform.position = new Vector3(1, 21, -4);
-
-
+            currentTetromino = Instantiate(cubeSelectPrefab[Random.Range(0, cubeSelectPrefab.Count)],tetrominoSpawn);
+            currentTetromino.tetrisManager = this;
+            index++;
         }
     }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "brick")
+        {
+            OnCollisionConsequence();
+        }
+    }
+
+    public void OnCollisionConsequence()
+    {
+        GenerateTetromino();
+    }
+
+  
 }
