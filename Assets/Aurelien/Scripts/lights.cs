39e01bd8162dff3bf290987d14877e9842d57da4
BonbonyAurelien <aurelien.bonbony@gmail.com>
2022-12-05 14:30:36 +0100

    jeu lights

---
+++ Assets/Aurelien/Scripts/lights.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Aurelien
+{
+    public class lights : MonoBehaviour
+    {
+        public Transform lumiere;
+        public bool dirGauche = false;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (dirGauche == false)
+            {
+                lumiere.position -= lumiere.right;
+                if (transform.position.z < -7755.6)
+                {
+                    dirGauche = true;
+                }
+            }
+            else
+            {
+                lumiere.position += lumiere.right;
+                
+                if (transform.position.z > -7616.2)
+                {
+                    dirGauche = false;
+                }
+            }
+
+
+        }
+    }
+}
+
