a50113486425ae7368f8f506bf65f6eef3d2126e
RENNES\a.motte <a.motte@rns.ecolescreatives.com>
2023-01-05 11:03:02 +0100

    DébutJeux3

---
+++ Assets/Alban/Scripts/Jeux_03/Mouvements.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Alban
+{
+    public class Mouvements : MonoBehaviour
+    {
+        public KeyCode myKey;
+        public Transform Viseur;
+        public float speedMax = 0.08f;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.S) == true)
+            {
+                Viseur.position += Viseur.forward * speedMax;
+            }
+
+            if (Input.GetKey(KeyCode.Q) == true)
+            {
+                Viseur.position += Viseur.right * -1 * speedMax;
+            }
+
+            if (Input.GetKey(KeyCode.Z) == true)
+            {
+                Viseur.position += Viseur.forward * -1 * speedMax;
+            }
+
+            if (Input.GetKey(KeyCode.D) == true)
+            {
+                Viseur.position += Viseur.right * speedMax;
+            }
+        }
+    }
+}
